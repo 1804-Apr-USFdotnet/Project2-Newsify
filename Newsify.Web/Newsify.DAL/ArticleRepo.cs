ba93c3e61f1c50dc3911160e720c2f34d7228aa9
jalmblade <j.almblade@gmail.com>
2018-05-16 21:46:15 -0400

    Added Repository implementation, added Validation method (but no implementation), added Repos for Article and Post.

---
+++ Newsify.Web/Newsify.DAL/ArticleRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newsify.DAL
+{
+    public class ArticleRepo : Repository<Article>, IArticleRepo
+    {
+        public ArticleRepo(NewsDBEntities context) : base(context)
+        {
+        }
+
+        public override bool Validation(Article entity)
+        {
+            //TODO: Validation of properties.
+            
+            return true;
+        }
+    }
+}
--- Newsify.Web/Newsify.DAL/IRepository.cs
+++ Newsify.Web/Newsify.DAL/IRepository.cs
@@ -9,7 +9,7 @@ using System.Data;
 
 namespace Newsify.DAL
 {
-    public interface IRepository<TEntity>
+    public interface IRepository<TEntity> where TEntity : class
     {
         DbContext DB { get; set; }
 
@@ -19,9 +19,11 @@ namespace Newsify.DAL
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> SearchFor(Expression<Func<TEntity, bool>> predicate);
 
-        TEntity Update(TEntity entity);
+        TEntity Update(int id, TEntity entity);
 
         TEntity Delete(TEntity entity);
         IEnumerable<TEntity> DeleteRange(IEnumerable<TEntity> entities);
+
+        bool Validation(TEntity entity);
     }
 }
