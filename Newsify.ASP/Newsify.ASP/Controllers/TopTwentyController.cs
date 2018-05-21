11c52a6bcca948f79041c2117db9ede048123501
Kyle Ott <kott@worcester.edu>
2018-05-21 18:56:18 -0400

    Cant display view of async

---
+++ Newsify.ASP/Newsify.ASP/Controllers/TopTwentyController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newsify.ASP.Models;
+
+namespace Newsify.ASP.Controllers
+{
+    public class TopTwentyController : Controller
+    {
+    TopTwenty top = new TopTwenty();
+    // GET: TopTwenty
+    public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TopTwenty/Details/5
+        public async Task<ActionResult> TopHome()
+        {
+            var name = await top.GetTopTwenty();
+            return View(await top.GetTopTwenty());
+        }
+        // GET: TopTwenty/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TopTwenty/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TopTwenty/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TopTwenty/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TopTwenty/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TopTwenty/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TopTwenty/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Newsify.ASP/Newsify.ASP/Models/TopTwenty.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+using System.Threading.Tasks;
+
+namespace Newsify.ASP.Models
+{
+
+    public class TopTwenty
+    {
+        public async Task<IEnumerable<Article>> GetTopTwenty()
+        {
+            string apiKey = "33b909af4e294034ad07bd3546790502"; // My NewsAPI key
+            var news = new NewsApiClient(apiKey);
+            var top2 = new TopHeadlinesRequest { Country = Countries.US };
+            var top = await news.GetTopHeadlinesAsync(top2);
+            Articles arts = new Articles();
+            Article art = new Article();
+
+            foreach (var article in top.Articles)
+            {
+                art.sourceName = article.Source.Name;
+                art.author = article.Author;
+                art.title = article.Title;
+                art.description = article.Description;
+                art.url = article.Url;
+                art.urlToImage = article.UrlToImage;
+                art.publishedAT = article.PublishedAt.ToString();
+
+                arts.data.Add(art);
+            }
+            return arts.data;
+        }
+
+}
+
+    public class Articles
+    {
+
+        public List<Article> data { get; set; }
+        public Articles() { data = new List<Article>(); }
+    }
+    public class Article
+        {
+            public string sourceName { get; set; }
+            public string author { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string url { get; set; }
+            public string urlToImage { get; set; }
+            public string publishedAT { get; set; }
+        }
+}
