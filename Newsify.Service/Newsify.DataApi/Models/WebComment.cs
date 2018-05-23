8fd7331609bc2b3f1f6fbc099726cfe255744b49
jalmblade <j.almblade@gmail.com>
2018-05-22 23:41:36 -0400

    Added Web models

--- Newsify.Service/Newsify.DataApi/Models/WebComment.cs
+++ Newsify.Service/Newsify.DataApi/Models/WebComment.cs
@@ -7,5 +7,10 @@ namespace Newsify.DataApi.Models
 {
     public class WebComment
     {
+        public int ID { get; set; }
+        public DateTime CommentedAt { get; set; }
+        public string comment1 { get; set; }
+        public DateTime Modified { get; set; }
+        public bool Active { get; set; }
     }
 }
---
+++ Newsify.Service/Newsify.DataApi/Models/WebUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Newsify.DataApi.Models
+{
+    public class WebUser
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
