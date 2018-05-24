f13723944bba23b7471d77a214a6fe382ca7060d
Manjinder Singh <manjinder360@yahoo.com>
2018-05-24 11:57:46 -0400

    Removed some properties that aren't going to be displayed to the user

--- Newsify.Service/Newsify.DataApi/Models/WebArticle.cs
+++ Newsify.Service/Newsify.DataApi/Models/WebArticle.cs
@@ -7,16 +7,9 @@ namespace Newsify.DataApi.Models
 {
     public class WebArticle
     {
-        public int ID { get; set; }
-        public Nullable<int> Source { get; set; }
-        public string Author { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
         public string UrlToImage { get; set; }
-        public DateTime PublishAt { get; set; }
-        public string Category { get; set; }
-        public string Topic { get; set; }
-        public bool Active { get; set; }
     }
 }
