bbc3a449a0851a4be687ace3ea75d0b1e490a846
Kyle Ott <kott@worcester.edu>
2018-05-25 18:57:23 -0400

    Added search for newsAPI

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import {Routes, RouterModule} from '@angular/router'
 import { ArticlesComponent } from './articles/articles.component';
 
 const appRoutes: Routes = [
-  {path: "articles", component: ArticlesComponent}
+  {path: "articles", component: ArticlesComponent},
+  {path: "articles/:id/:input", component: ArticlesComponent},
+  {path: '', redirectTo: '/articles', pathMatch: 'full' },
 ]
 
 @NgModule({
+  exports: [ RouterModule ],
   imports: [
     CommonModule, 
     RouterModule.forRoot(appRoutes)
